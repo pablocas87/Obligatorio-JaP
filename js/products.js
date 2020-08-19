381e15323e45b258deaccdd9a81fd3c43caec4b2
pablocas87 <68858954+pablocas87@users.noreply.github.com>
2020-08-19 16:05:33 -0300

    prods

--- js/products.js
+++ js/products.js
@@ -2,7 +2,7 @@ let minCount = undefined;
 let maxCount = undefined;
 let currentProductArray = [];
 let ORDER_ASC_BY_COST = "AZ";
-let ORDER_DESC_BY_COST = "ZA";
+let ORDER_DESC_BY_COST = "ZA"
 
 function sortAndShowProducts(sort){
     currentProductArray = sortProducts(sort,currentProductArray);
