c9a09f15bdcbe057d0b3b524cbdb2abb2095b590
pablocas87 <68858954+pablocas87@users.noreply.github.com>
2020-09-07 13:51:51 -0300

    Coment

--- js/product-info.js
+++ js/product-info.js
@@ -1,5 +1,4 @@
 var product = {};
-var comentario = {};
 
 function showImagesGallery(array){
     let imagenes = "";
@@ -16,24 +15,7 @@ function showImagesGallery(array){
     document.getElementById("productImagesGallery").innerHTML = imagenes;
    
 }
-//Comentarios
-/**
-function showComments(array){
-    let comments = "";
-    for (let i = 0; i < array.length; i++){
-        let comment = array[i];
-        comments +=  `<div class="col-lg-3 col-md-4 col-6">
-            <div class="d-block mb-4 h-100">
-                <img class="img-fluid img-thumbnail" src="`+comment+`" alt="">
-            </div>
-        </div>`;
-    }
-    
-    
-    document.getElementById("productComments").innerHTML = comments;
-   
-}
-*/
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
@@ -51,20 +33,3 @@ function datos(product){
     document.getElementById("productCount").innerHTML = product.currency + " " + product.cost;
     document.getElementById("productName").innerHTML = product.name;
 }
-/**
-//Comentarios
-document.addEventListener("DOMContentLoaded", function(e){
-    getJSONData(PRODUCT_INFO_COMMENTS_URL).then(function(resultObj){
-        if (resultObj.status === "ok"){
-           comentario = resultObj.data;
-           datos(comentario);
-           showComments(comentario.description)
-        }
-    });
-});
-function datos(product){
-    document.getElementById("userComment").innerHTML = comentario.description;
-    document.getElementById("commentUser").innerHTML = comentario.user + " " + comentario.dateTime;
-    document.getElementById("productScore").innerHTML = product.score;
-}  
- */
