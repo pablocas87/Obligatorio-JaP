569af2fc7d6c9f8e482d654d95399c1bfcd1ed5b
pablocas87 <68858954+pablocas87@users.noreply.github.com>
2020-09-07 14:12:22 -0300

    coments

--- js/product-info.js
+++ js/product-info.js
@@ -1,4 +1,5 @@
 var product = {};
+var comentarios ={};
 
 function showImagesGallery(array){
     let imagenes = "";
@@ -16,6 +17,27 @@ function showImagesGallery(array){
    
 }
 
+
+//comentarios 
+
+function showComments(array){
+    let comments = "";
+    for (let i = 0; i < array.length; i++){
+        let comentario = array[i];
+        comments +=  `<div class="col-lg-3 col-md-4 col-6">
+            <div class="d-block mb-4 h-100">
+                <img class="img-fluid img-thumbnail" src="`+comentario+`" alt="">
+            </div>
+        </div>`;
+    }
+    
+    
+    document.getElementById("productComments").innerHTML = comments;
+   
+}
+//comentarios
+
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
@@ -27,9 +49,27 @@ document.addEventListener("DOMContentLoaded", function(e){
            showImagesGallery(product.images)
         }
     });
+
+//comentarios
+
+    getJSONData(PRODUCT_INFO_COMMENTS_URL).then(function(resultObj){
+        if (resultObj.status === "ok"){
+         comentarios = resultObj.data;
+         datos1(comentarios);
+         showComments(comentarios.description)
+        }
+    });
+//comentarios
 });
 function datos(product){
     document.getElementById("productDescription").innerHTML = product.description;
     document.getElementById("productCount").innerHTML = product.currency + " " + product.cost;
     document.getElementById("productName").innerHTML = product.name;
+}
+//comentarios
+function datos1(comentarios){
+    document.getElementById("productComment").innerHTML = comentarios.description;
+    document.getElementById("userComment").innerHTML = comentarios.user;
+    document.getElementById("productScore").innerHTML = comentarios.score + "" + comentarios.dateTime;
+//comentarios
 }
